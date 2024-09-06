326cbc738d5b2375bddb8be8f6958e645f3a0fd9
Rafael Santos <rafael.agt.santos@gmail.com>
2024-09-06 16:08:45 -0300

    Feat: Dynamically changing card general, positive and negative meanings

--- Projeto/pages/tarot-card.js
+++ Projeto/pages/tarot-card.js
@@ -33,6 +33,18 @@ function renderPageInformation(selectedCard) {
   // Preenche informações de palavras-chave da carta
   const cardKeywordsElement = document.getElementById("card-keywords");
   cardKeywordsElement.innerHTML = `<p><strong>Palavras-chave: </strong>${selectedCard.keywords}</p>`;
+
+  // Preenche informações de significado geral da carta
+  const cardGeneralMeaningElement = document.getElementById("card-general-meaning");
+  cardGeneralMeaningElement.innerHTML = renderListItem(selectedCard.meaning.general);
+
+  // Preenche informações de lado positivo da carta
+  const cardPositiveMeaningElement = document.getElementById("card-positive-meaning");
+  cardPositiveMeaningElement.innerHTML = renderListItem(selectedCard.meaning.positive);
+
+  // Preenche informações de lado negativo da carta
+  const cardNegativeMeaningElement = document.getElementById("card-negative-meaning");
+  cardNegativeMeaningElement.innerHTML = renderListItem(selectedCard.meaning.negative);
 }
 
 // Essa função recebe uma lista de textos e retorna cada item da lista em um elemento <p> HTML
@@ -42,6 +54,19 @@ function renderParagraphs(textList){
   for(item of textList) {
     textResult+= `
       <p>${item}</p>
+    `;
+  }
+
+  return textResult;
+}
+
+// Essa função recebe uma lista de textos e retorna cada item da lista em um elemento <li> HTML
+function renderListItem(textList){
+  let textResult = "";
+
+  for(item of textList) {
+    textResult+= `
+      <li>${item}</li>
     `;
   }
 
