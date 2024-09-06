658a8bb38c031b02ebd4ce5bc8ff983d532c39b0
Rafael Santos <rafael.agt.santos@gmail.com>
2024-09-06 11:52:21 -0300

    Feat: Creating final project folder

---
+++ Projeto Completo/data.js
@@ -0,0 +1,20 @@
+let tarotCardsList = [
+  {
+      name: "0 - O Louco",
+      img: "./assets/images/cards/major-arcana/0-the-fool.jpg",
+      keywords: "Novos começos, aventura, espontaneidade, intuição, liberdade, crescimento pessoal, inocência, riscos, oportunidades.",
+      link: "https://pt.wikipedia.org/wiki/O_Louco"
+  },
+  {
+      name: "I - O Mago",
+      img: "./assets/images/cards/major-arcana/1-the-magician.jpg",
+      keywords: "Criação, poder, manifestação, habilidade, iniciativa, confiança, foco, mente, realização.",
+      link: "https://pt.wikipedia.org/wiki/O_Mago_(tar%C3%B4)"
+  },
+  {
+      name: "II - A Sacerdotisa",
+      img: "./assets/images/cards/major-arcana/2-the-high-priestess.jpg",
+      keywords: "Intuição, Sabedoria, Mistério, Espiritualidade, Conexão, Intuito, Profundidade, Paz, Iluminação.",
+      link: "https://pt.wikipedia.org/wiki/A_Suma_Sacerdotisa"
+  }
+];
---
+++ Projeto Completo/database/major-arcana/0-the-fool.js
@@ -0,0 +1,37 @@
+// Declara as informações da nossa carta em uma constante
+const THE_FOOL = {
+  name: "0 - O Louco",
+  img: "./assets/images/cards/major-arcana/0-the-fool.jpg",
+  description:[
+    "O Louco é uma carta que desperta muitas curiosidades e interpretações. Representa o início de uma jornada, um novo ciclo e a disposição para abraçar o desconhecido. É a carta da aventura, da espontaneidade e da liberdade.",
+    "O louco pode ser associado a figura do bobo da corte. Ambos representam a liberdade de ir além das convenções sociais, a capacidade de enxergar a verdade por trás das aparências e a jornada interior. O bobo da corte, com sua aparente loucura, era muitas vezes um porta-voz da verdade, enquanto o Louco no Tarot simboliza a ruptura com o conhecido e a busca por novas perspectivas.",
+  ],
+  numerology: [
+    "A numerologia atribui ao Louco, tanto o número 0 quanto o XXII. O zero simboliza o início, o vazio e o potencial infinito, representando um novo começo livre de limitações. Já o XXII indica a jornada completa, a integração dos opostos e a transcendência, simbolizando a busca por uma compreensão mais profunda da realidade.",
+    "A dualidade entre esses números reflete a natureza paradoxal do Louco, que é tanto o ponto de partida quanto o destino final. Essa combinação numérica enfatiza a ideia de que o Louco representa um ciclo constante de criação e destruição, de início e fim, de potencial e realização.",
+  ],
+  meaning: {
+    general: [
+      "Novas Começos: O Louco simboliza o início de algo novo, seja um projeto, um relacionamento ou uma nova fase da vida.",
+      "Intuição e Espontaneidade: A carta incentiva a confiar na intuição e a agir de forma impulsiva, mas positiva. É um convite para deixar de lado os planos e seguir o fluxo da vida.",
+      "Crescimento Pessoal: O Louco representa a necessidade de sair da zona de conforto e buscar novas experiências para evoluir como pessoa.",
+      "Inocência e Pureza: A carta traz consigo uma energia de pureza e inocência, convidando a enxergar o mundo com olhos de criança.",
+      "Riscos e Oportunidades: O Louco nos lembra que a vida é cheia de riscos, mas também de grandes oportunidades. É preciso ter coragem para seguir em frente.",
+    ],
+    positive: [
+      "Abrace a Novidade: O Louco nos incentiva a sermos mais abertos a novas experiências, ideias e oportunidades. É hora de deixar para trás velhos hábitos e crenças limitantes.",
+      "Confie em sua Intuição: A intuição é a bússola do Louco. Siga seu coração e confie em seus instintos, mesmo que pareçam contraditórios.",
+      "Seja Espontâneo: A vida é uma aventura, e o Louco nos convida a viver cada momento com intensidade e espontaneidade.",
+      "Não Tenha Medo de Errar: Errar faz parte da jornada. Ao invés de temer o fracasso, aprenda com seus erros e siga em frente.",
+      "Acredite em Si Mesmo: O Louco representa a autoconfiança e a crença em suas próprias capacidades. Acredite em seu potencial e vá em busca dos seus sonhos.",
+    ],
+    negative: [
+      "Impulsividade: O Louco pode indicar impulsividade e a tendência a tomar decisões sem pensar nas consequências. É importante equilibrar a espontaneidade com a reflexão.",
+      "Falta de Planejamento: A carta pode indicar falta de planejamento e a tendência a se jogar de cabeça em novas aventuras sem ter uma ideia clara do que deseja.",
+      "Negligência: O Louco pode indicar negligência com as responsabilidades e a tendência a se deixar levar pela emoção.",
+      "Teimosia: A carta pode indicar teimosia e a dificuldade em aceitar conselhos. É importante estar aberto a diferentes perspectivas.",
+      "Medo do Compromisso: O Louco pode indicar medo de se comprometer e de construir algo duradouro.",
+    ],
+  },
+  keywords: "Novos começos, aventura, espontaneidade, intuição, liberdade, crescimento pessoal, inocência, riscos, oportunidades.",
+}
