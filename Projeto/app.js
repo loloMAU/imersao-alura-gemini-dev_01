8d6a2e58f35a5504600789da3d31a39eace71db2
Rafael Santos <rafael.agt.santos@gmail.com>
2024-09-07 19:59:18 -0300

    Feat: Adding luck of the day logic

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,73 +1,31 @@
-function searchCard() {
-  // Aqui utilizamos uma maneira diferente de declarar variaveis.
-  // Usar "const" é uma boa prática quando queremos que nossa variável não seja alterada acidentalmente
-  // Quer saber mais? Pergunte ao Gemini!
-  // - Qual a diferença de declarar variáveis "let" e "const" no javascript?
+function luckOfTheDay() {
+  const nameInputValue = document.getElementById("name-input").value;
+  const dateInputValue = document.getElementById("date-input").value;
+  const today = new Date().toDateString();
 
-  // Obtém o elemento em que exibiremos os resultados de busca
-  const resultListElement = document.getElementById("search-results-list");
-  // Obtém valor digitado pelo usuário no campo de texto
-  const searchInputValue = document.getElementById("search-input").value;
-
-  // Caso o campo de texto esteja vazio, retornamos uma mensagem ao usuário
-  if (!searchInputValue) {
-    resultListElement.innerHTML = "<p class='text-center'>Digite algum termo para buscar resultados</p>";
+  if(!nameInputValue || !dateInputValue) {
+    alert("Por favor, digite seu nome completo e data de nascimento para tirar a sorte do dia!");
     return;
   }
 
-  // Aqui usamos uma função diferente para nos ajudar a filtrar os dados.
-  // As listas no javascript possuem um método chamado .filter() para esses casos!
-  // Quer saber mais? Pergunte ao Gemini!
-  // - Como funciona o método .filter() de uma lista no javascript?
-  // - O que é uma "arrow function" no javascript?
-  const filteredData = DATABASE.filter(
-    // O método .filter() executa essa função dentro dele para cada elemento da lista,
-    // assim como o "for(let dado of dados)" da aula.
-    // Essa função retorna um valor VERDADEIRO ou FALSO para cada item da lista,
-    // assim como o if() que usamos na aula para determinar se tinhamos que exibir
-    // ou não o resultado na tela.
-    function filterResults(entry){
-      let name = entry.name.toLowerCase();
-      let keywords = entry.keywords.toLowerCase();
-      let value = searchInputValue.toLowerCase();
-      // Retorna se devemos ou nao incluir o resultado na lista
-      return name.includes(value) || keywords.includes(value);
-    }
-  )
+  const luckString = `${nameInputValue}${dateInputValue}${today}`;
+  const luckSigil = btoa(luckString);
+  const cardNumber = drawCardFromSigil(luckSigil);
 
-  // Caso nossa lista filtrada esteja vazia, retornamos uma mensagem ao usuário
-  // O ".length" retorna o tamanho de uma lista no javascript.
-  if (!filteredData.length) { // Podemos ler isso como "Se a lista não tiver tamanho faça isso"
-    // Nessa linha incluimos o termo digitado pelo usuário na mensagem usando o dinheirinho ${}!
-    resultListElement.innerHTML = `<p class="text-center">Nenhum resultado encontrado para "${searchInputValue}" </p>`;
-    return;
+  window.location.href = `/pages/tarot-card.html?number=${cardNumber}`;
+}
+
+function drawCardFromSigil(sigil) {
+  const sigilNumerology = getNumberFromSigil(sigil);
+  const cardNumber = (sigilNumerology % DATABASE.length) - 1;
+
+  return cardNumber;
+}
+
+function getNumberFromSigil(str) {
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += str.charCodeAt(i);
   }
-
-  // Repare que no filtro acima, não temos nenhum HTML. Isso porque apenas filtramos nossa
-  // lista de dados e removemos os itens que não devem ser exibidos. Agora precisamos exibir
-  // esses resultados filtrados na tela.
-  // Para isso vamos usar mesmo método for() criado na aula anterior, sem precisar alterar nada
-  // da lógica para usar o if() dentro dele, pois nossa lista já está filtrada.
-  // Alteramos apenas os dados declarados no for() para usar nossa lista já filtrada.
-
-  // Variável que guarda nossos resultados de busca
-  let searchResult = "";
-  // Monta nossa lista HTML de resultados
-  for (let entry of filteredData) { // Mudamos apenas aqui de "DATABASE" para "filteredData"
-    let link = `/pages/tarot-card.html?number=${filteredData.indexOf(entry)}`;
-    searchResult += `
-      <a class="card card--xs" href="${link}">
-        <div class="h-layout">
-          <img style="width: 100px;" src="${entry.img}"/>
-          <div class="v-layout middle fill-height fill-width">
-            <div class="font-size-lg">${entry.name}</div>
-            <p><strong>Palavras-chave:</strong> ${entry.keywords}</p>
-          </div>
-        </div>
-      </a>
-    `;
-  }
-
-  // Exibindo os resultados no HTML
-  resultListElement.innerHTML = searchResult;
+  return sum;
 }
